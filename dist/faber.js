6f17ea7486d9f4d050ac2bd0581f314638a0d0cb
Viktor Charypar <charypar@gmail.com>
2014-08-18 14:18:03 +0100

    Export MediumEditorExtended as well

--- dist/faber.js
+++ dist/faber.js
@@ -2385,6 +2385,7 @@ if('undefined' !== typeof module && 'undefined' !== typeof module.exports) {
   module.exports = {
     Editor: window.faber,
     OrderedListComponent: OrderedListComponent,
+    MediumEditorExtended: MediumEditorExtended,
     RichTextComponent: RichTextComponent
   }
 }
